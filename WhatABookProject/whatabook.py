3995311bda2e4c505e9f59027235bbbd5c353458
kinikepike1 <99456793+kinikepike1@users.noreply.github.com>
2023-11-18 09:10:38 -0600

    Update whatabook.py

--- WhatABookProject/whatabook.py
+++ WhatABookProject/whatabook.py
@@ -5,7 +5,7 @@ import re
 
 config = {
     "user":"whatabook_user",
-    "password":"MySQL8IsGreat!",
+    "password":"**************",
     "host":"127.0.0.1",
     "database":"whatabook",
     "raise_on_warnings":True
